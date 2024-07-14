2f8c2e3c7255a70f9a615f84f2a70a110408eba7
jimmaastricht <jimmaastricht5@gmail.com>
2024-07-14 16:40:30 -0500

    source code matches cloud function, duplicate file in birdclassifier project moved to archive

--- main.py
+++ main.py
@@ -19,6 +19,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+# 7/14/2024 matches google cloud version
 import pandas as pd
 from datetime import datetime
 from datetime import timedelta
